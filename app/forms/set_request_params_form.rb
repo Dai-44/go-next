47da740e1a71f1baa6dfdf0a60b540e37b7bba85
Dai <kobayashi.daigo3@gmail.com>
2023-12-07 16:42:09 +0900

    add: 行き先探しでの条件選択フォームのためのFormオブジェクト作成

---
+++ app/forms/set_request_params_form.rb
@@ -0,0 +1,13 @@
+class SetRequestParamsForm
+
+  include ActiveModel::Model
+  include ActiveModel::Attributes 
+  
+  attribute :type, :string
+  attribute :feeling, :string
+
+  with_options presence: true do
+    validates :type
+    validates :feeling
+  end
+end
