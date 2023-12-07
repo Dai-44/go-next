d570fcf99601b768d22f36af2bbff185d46a791c
Dai <kobayashi.daigo3@gmail.com>
2023-12-07 21:45:51 +0900

    add: 条件選択画面(searches/new.html)でのフォーム実装

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,12 @@
 class SearchesController < ApplicationController
   def index
   end
+
+  def new
+    @user_selection = SetRequestParamsForm.new
+  end
+
+  def create
+
+  end
 end
--- app/forms/set_request_params_form.rb
+++ app/forms/set_request_params_form.rb
@@ -1,5 +1,4 @@
 class SetRequestParamsForm
-
   include ActiveModel::Model
   include ActiveModel::Attributes 
   
