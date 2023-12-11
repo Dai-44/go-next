c3f362b92cfc1a0d44560c69e22873908a22bdee
Dai <kobayashi.daigo3@gmail.com>
2023-12-11 15:47:03 +0900

    add: アプリとDBのデフォルトタイムゾーンをJSTに設定

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,9 @@ module GoNext
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
