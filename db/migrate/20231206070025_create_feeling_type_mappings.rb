831e994b3540cdf96675e229ae7448992d39da72
Dai <kobayashi.daigo3@gmail.com>
2023-12-12 17:32:16 +0900

    remove: デプロイエラー解消のため、中間テーブル作成時に外部キー制約が有効化されないよう修正

--- db/migrate/20231206070025_create_feeling_type_mappings.rb
+++ db/migrate/20231206070025_create_feeling_type_mappings.rb
@@ -1,8 +1,8 @@
 class CreateFeelingTypeMappings < ActiveRecord::Migration[7.1]
   def change
     create_table :feeling_type_mappings do |t|
-      t.references :feeling, null: false, foreign_key: true
-      t.references :google_places_api_type, null: false, foreign_key: true
+      t.references :feeling, null: false
+      t.references :google_places_api_type, null: false
 
       t.timestamps
     end
