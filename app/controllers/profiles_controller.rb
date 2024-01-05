e9caaf6b746cffe17d454eaabae14254507288e6
Dai <kobayashi.daigo3@gmail.com>
2024-01-05 19:27:15 +0900

    feat: プロフィール編集機能の実装

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,26 @@
 class ProfilesController < ApplicationController
+  before_action :set_user, only: %i[edit update]
+
   def show; end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to profile_path, flash: { success: 'プロフィールを更新しました' }
+    else
+      flash.now[:danger] = 'プロフィールの更新に失敗しました'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   get "oauth/callback" => "oauths#callback"
   get "oauth/:provider" => "oauths#oauth", as: :auth_at_provider
 
-  resource :profile, only: %i[show]
+  resource :profile, only: %i[show edit update]
 
   resources :password_resets, only: %i[new create edit update]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
