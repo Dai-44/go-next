a0440c62b556490f140e9339a1648786618c0c55
Dai <kobayashi.daigo3@gmail.com>
2024-01-09 09:42:32 +0900

    fix: ドライブ履歴の作成はログインユーザーのみの機能であるためログイン状態でのみリンクが表示されるよう修正、その他動作確認と修正

--- app/controllers/drive_records_controller.rb
+++ app/controllers/drive_records_controller.rb
@@ -1,11 +1,14 @@
 class DriveRecordsController < ApplicationController
   def create
+    # 目的地となる場所の特定と、その場所へのユーザーのドライブ履歴を生成するLogDestinationActivityServiceのcallメソッドを実行
     service = LogDestinationActivityService.new(
       current_user.id,
       destination_params,
       record_type: 'drive_record'
     )
-    service.call
+    if service.call
+      redirect_to root_path, flash: { success: "ドライブ履歴を保存しました" } # 処理後は仮でルートパスに遷移しているが、実際は経路案内を表示する画面に遷移させる。画面のUI/UXを整える過程で修正する。
+    end
   end
 
   private
--- app/services/log_destination_activity_service.rb
+++ app/services/log_destination_activity_service.rb
@@ -1,12 +1,13 @@
 class LogDestinationActivityService
-  def initialize(user_id, destination_info, record_type:)
+  def initialize(user_id, destination_info, record_type:) # drive_recordsコントローラでこのサービスオブジェクトを呼び出す際に、引数の示す内容が不明瞭にならないようにする意図で、record_typeをキーワード引数(デフォルト値は無し)として定義している。
     @user = User.find(user_id)
     @destination_info = destination_info
     @record_type = record_type
   end
 
+  # 目的地となる場所の特定と、その場所へのユーザーのドライブ履歴を生成する処理
   def call
-    type = Type.find_by(name: @destination_info[:type])
+    type = GooglePlacesApiType.find_by(name: @destination_info[:type])
     destination = Destination.find_or_create_by(name: @destination_info[:name], address: @destination_info[:address], latitude: @destination_info[:latitude], longitude: @destination_info[:longitude], google_places_api_type: type)
     create_drive_record(destination)
   end
@@ -14,6 +15,6 @@ class LogDestinationActivityService
   private
 
   def create_drive_record(destination)
-    @user.drive_records.create(destination: destination)
+    @user.drive_records.create(destination: destination) # 基本的にはdrive_recordsレコードの保存処理の失敗は想定されないが、予期せぬ原因で失敗した場合の対応も追って記述しておく。
   end
 end
