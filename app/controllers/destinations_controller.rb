7995ddaa167cb10f911e148e5622137332229271
Dai <kobayashi.daigo3@gmail.com>
2024-01-10 19:33:19 +0900

    feat: ブックマーク詳細表示機能の実装

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,4 +1,8 @@
 class DestinationsController < ApplicationController
+  def show
+    @bookmark_destination = current_user.bookmark_destinations.find(params[:id])
+  end
+
   def bookmarks
     @bookmark_destinations = current_user.bookmark_destinations.order(created_at: :desc).page(params[:page])
   end
