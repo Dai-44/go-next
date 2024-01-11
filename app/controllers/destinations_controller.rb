d59dcb5c01ce19e6b4d78597dd0beddd8aa6e5a4
Dai <kobayashi.daigo3@gmail.com>
2024-01-11 23:05:02 +0900

    feat: ブックマーク一覧ページでの検索フォームと検索処理を実装

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,9 +1,14 @@
 class DestinationsController < ApplicationController
+  include Searchable
+
   def show
     @bookmark_destination = current_user.bookmark_destinations.find(params[:id])
   end
 
   def bookmarks
-    @bookmark_destinations = current_user.bookmark_destinations.order(created_at: :desc).page(params[:page])
+    @q = current_user.bookmark_destinations.ransack(params[:q])
+    @bookmark_destinations = @q.result(distinct: true).order(created_at: :desc).page(params[:page])
+    # 検索時の選択肢となるデータを用意
+    load_search_options
   end
 end
