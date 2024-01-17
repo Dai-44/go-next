909e9c91228edaa6a8de2e937143629284ba76f8
Dai <kobayashi.daigo3@gmail.com>
2024-01-18 01:08:15 +0900

    fix: ドライブ履歴一覧およびブックマーク一覧ページでの検索において、検索フォームの選択肢にはユーザーに紐づいていない場所のカテゴリ等も含めて全て表示されており検索の体験が悪かったため、ログインユーザーのドライブ履歴およびブックマークのデータに関連するものしか表示されないように修正

--- app/controllers/concerns/searchable.rb
+++
@@ -1,9 +0,0 @@
-module Searchable
-  extend ActiveSupport::Concern
-
-  # 検索時の選択肢となるデータを取得してインスタンス変数に格納する処理
-  def load_search_options
-    @areas = Destination.get_unique_areas
-    @types = GooglePlacesApiType.get_display_name
-  end
-end
