ce75c3d544ac68cfc3f1e4aa6d87b308c42bda15
Dai <kobayashi.daigo3@gmail.com>
2024-01-18 20:43:42 +0900

    feat: 取得した独自ドメインを反映させるための設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,10 +82,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts << 'www.your-next-destination.com'
+
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
